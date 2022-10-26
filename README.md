55a4786dfdb331d120ceeddc5a284d7bf91d9df8
daduranka <116219580+daduranka@users.noreply.github.com>
2022-10-26 11:54:47 -0600

    Introduction/documentation steps

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 #student name: Braden Brown 
 
 #Project: VLSI design of a vending machine controller
+
+This project entails designing a vending machine controller starting from Verilog and ending with tape out. 
+The vending machine will have 5 functions: 
+    1. Reading the user’s selection 
+    2. Counting coins and displaying current values on a BCD display 
+    3. Dispensing food selection after checking correct amount of money is deposited 
+    4. Checking if the food has been dispensed (assuming a pressure sensor is connected to the controller) 
+       and dispensing until the food has been released 
+    5. Dispensing change after food has been released. 
+ 
+It is assumed that the vending machine this controller will work with has 4 rows with 5 options on each row making a possible 20 selections. Each selection will have two identifiers a row identifier (A, B, C, D) and a column identifier (1, 2, 3, 4, 5) with each row having a different set price.
+
---
+++ tb_vendingMachine.v
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ECE 6710 VLSI Project: Vending Machine Controller
+// Student: Braden Brown
+//
+// Description: This code represents the testbench of a Vending Machine controller (see vendingMachine.v). 
+// The vending machine will have 5 functions: 
+// 1. Reading the user’s selection 
+// 2. Counting coins and displaying current values on a BCD display 
+// 3. Dispensing food selection after checking correct amount of money is deposited 
+// 4. Checking if the food has been dispensed (assuming a pressure sensor is connected to the controller) 
+//    and dispensing until the food has been released 
+// 5. Dispensing change after food has been released. 
+// 
+// I will also assume that the vending machine has 4 rows with 5 options on each row making a possible 20 selections. 
+// Each selection will have two identifiers a row identifier (A, B, C, D) and a column identifier (1, 2, 3, 4, 5) 
+// with each row having a different price. 
+//
+// This program attempts to test as many of the possible inputs as possible to verify correct operation. 
+// With two five bit inputs this results in a minumum of 2^10 or 1024 inputs. 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
